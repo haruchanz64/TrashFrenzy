2f35c2f061353334d6e193e473bb60b0b1b595c6
Haruki <radam.aronjake@gmail.com>
2024-10-04 14:03:54 +0800

    Added CreateAssetMemu header cause I forgot

--- Assets/Scripts/ScriptableObjects/WeaponType.cs
+++ Assets/Scripts/ScriptableObjects/WeaponType.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
 public class WeaponType : ScriptableObject
 {
     [Header("Weapon Info")]
