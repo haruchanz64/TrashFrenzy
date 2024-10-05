4bb44ae741cfe09cea8be3f08049c89f64470abf
Haruki <radam.aronjake@gmail.com>
2024-10-05 16:43:34 +0800

    Made Weapon script to be blanked first.

--- Assets/Scripts/Core/Weapon.cs
+++ Assets/Scripts/Core/Weapon.cs
@@ -4,27 +4,6 @@ namespace TrashFrenzy.Core
 {
     public class Weapon : MonoBehaviour
     {
-        [Header("Weapon")]
-        [SerializeField] private WeaponType[] weapons;
-        [SerializeField] private WeaponType currentWeapon;
-        [SerializeField] private Transform weaponHolder;
-
-        [Header("Weapon System")]
-        [SerializeField] private float weaponSwitchDelay = 0.5f;
-        [SerializeField] private bool canSwitchWeapon = true;
-        [SerializeField] private int currentWeaponIndex = 0;
-        [Header("Components")]
-        [SerializeField] private InputActionAsset inputActionAsset;
-
-        public void GetCurrentWeapon()
-        {
-            
-        }
-
-        private void EnableWeaponSwitch()
-        {
-            canSwitchWeapon = true;
-        }
 
     }
 }
