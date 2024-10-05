4306af878fc16fb7f51e34fb7d0695f01f2d881d
Haruki <radam.aronjake@gmail.com>
2024-10-05 16:45:21 +0800

    Added UIManager

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using TrashFrenzy.Core;
+using TrashFrenzy.Mechanics;
+using UnityEngine;
+using UnityEngine.UI;
+namespace TrashFrenzy.Manager
+{
+    public class UIManager : MonoBehaviour
+    {
+
+    }
+}
